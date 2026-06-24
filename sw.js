49649f84b9e59370d71b87ba352e0a00681abd8a
Justin <justmo0225@gmail.com>
2026-06-24 00:39:56 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'climb-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
