92d569fae7cef45077ded352c3a420b55684f815
Justin <justmo0225@gmail.com>
2026-06-24 02:05:16 -0700

    Add files via upload
    massive update
    v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'climb-v1';
+const CACHE = 'climb-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
@@ -7,11 +7,21 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy)).catch(()=>{});
+        return resp;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
